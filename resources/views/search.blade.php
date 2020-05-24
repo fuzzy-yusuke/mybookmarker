0ce13296c468657c2861e19e98baefcce1944457
fuzzy-yusuke <youth_k0228@outlook.com>
2020-05-24 23:32:27 +0900

    #2

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,10 +1,10 @@
 {{Form::open (['method' => 'get']) }}
     {{ csrf_field()}} <!-- クロスサイトリクエストフォージェリによる不正リクエストを防ぐ -->
-<div class='form-group' style="position:absolute; top:0px; right:200px">
+<div class='form-group' style="position:absolute; top:0px; right:159px">
     {{Form::text('keyword',null,['class'=>'form-control','placeholder'=>'キーワードを入力'])}}
 </div>
 
 <div class style="position:absolute; top:0px; right:100px">
-    {{Form::submit('検索',['class'=>'btn btn-outline-primary'])}}
+    {{Form::submit('検索',['class'=>'btn btn-primary'])}}
 </div>
 {{Form::close()}}
